c3a8a74cc07bd06e19f59a3cb977f61e1b60c83c
Francois Polo <andasan@gmail.com>
2020-08-25 11:28:14 -0700

    applied styled components to pages

--- src/pages/sign-in-and-sign-up/SignInAndSignUpPage.jsx
+++ src/pages/sign-in-and-sign-up/SignInAndSignUpPage.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import SignIn from '../../components/sign-in/sign-in.component';
-import SignUp from '../../components/sign-up/sign-up.component';
+// import './SignInAndSignUpPage.scss';
+import {SignInAndSignUpContainer} from './SignInAndSignUpPage.styles';
 
-import './SignInAndSignUpPage.scss';
+import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInAndSignUpPage = () => (
-  <div className='sign-in-and-sign-up'>
+  <SignInAndSignUpContainer>
     <SignIn />
     <SignUp />
-  </div>
+  </SignInAndSignUpContainer>
 );
 
 export default SignInAndSignUpPage;
